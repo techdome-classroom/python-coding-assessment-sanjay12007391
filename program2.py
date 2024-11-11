fd7c7d6c0e0fd2d1f787e949113fe734b7ac0811
sanjay12007391 <sanjaysomishetty01@gmail.com>
2024-11-11 15:54:22 +0530

    Auto commit on save

--- program2.py
+++ program2.py
@@ -11,7 +11,7 @@ def decode_message( s: str, p: str) -> bool:
         for j in range(1, n + 1):
                 if p[j - 1] == s[i - 1] or p[j - 1] == '?':
                         dp[i][j] = dp[i - 1][j - 1]
-                        elif p[j - 1] == '*':
+                elif p[j - 1] == '*':
                         dp[i][j] = dp[i][j - 1] or dp[i - 1][j]
 
         return dp[m][n]
